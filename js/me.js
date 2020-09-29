d627a67c31a477cdbd1b11f675ddfee534350e93
laith206 <67703195+laith206@users.noreply.github.com>
2020-09-29 14:47:50 +0300

    Initial commit

---
+++ js/me.js
@@ -0,0 +1,56 @@
+
+
+/*---- hiden-show function  ----*/
+$("#all").click(function () {
+    $(".one").first().show("fast", function showNext() {
+        $(this).next(".one,.two,.thre,.four").show("fast", showNext);
+    });
+});
+
+
+$("#one").click(function () {
+    $(".one").show(function showNext() {
+        $(".two,.thre,.four").hide(1000);
+    });
+});
+
+$("#hidr").click(function () {
+    $(".one,.two,.thre,.four").hide(1000);
+});
+
+
+$("#two").click(function () {
+    $(".two,.thre").show(function showNext() {
+        $(".one,.four").hide(1000);
+    });
+});
+
+$("#thre").click(function () {
+    $(".thre,.four").show(function showNext() {
+        $(".one,.two").hide(1000);
+    });
+});
+
+$("#four").click(function () {
+    $(".one,.four").show(function showNext() {
+        $(".thre,.two").hide(1000);
+    });
+});
+
+
+const nav = document.querySelector('.navbar');
+
+window.onscroll = function () {
+    var top = window.scrollY;
+    console.log(top);
+    if (top >= 900) {
+        nav.classList.add('activ-aa')
+    } else {
+        nav.classList.remove('activ-aa')
+    }
+}
+
+
+
+
+
